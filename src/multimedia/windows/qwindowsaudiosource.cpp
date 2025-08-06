cd53f067a19996e4dfc6644e65b0e27736b17162
Tim Blechmann <tim.blechmann@qt.io>
2025-08-06 13:24:46 +0800

    Audio: stream - introduce invokeOnAppThread helper
    invokeOnAppThread will invoke the functor on the application thread, but
    will cancel pending actions if the stream object is destroyed.
    
    Change-Id: I5b59fba55c37ee092171034ae74e14795e6bae54
    Reviewed-by: Artem Dyomin <artem.dyomin@qt.io>
    (cherry picked from commit e7d47b5561d97c7eb5cf9ae7329aacadfc69f4dd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit b939bd4c4bc03dbf9eccb30d92ce59477fee1a05)
    (cherry picked from commit b68ab51b519735598dff222e4de42fe88843408f)

--- src/multimedia/windows/qwindowsaudiosource.cpp
+++ src/multimedia/windows/qwindowsaudiosource.cpp
@@ -319,7 +319,7 @@ void QWASAPIAudioSourceStream::handleAudioClientError()
     audioClientReset(m_audioClient);
     requestStop();
 
-    QMetaObject::invokeMethod(&m_ringbufferDrained, [this] {
+    invokeOnAppThread([this] {
         handleIOError(m_parent);
     });
 }
